cd6d0b37a799ca7ecfb87f676f0f322920232453
Torin Bolander <bol19001@byui.edu>
2023-11-13 21:31:04 -0700

    Week05 BoM activity

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,25 @@
+const addChapter = document.querySelector("button");
+const topTen = document.querySelector("#list");
+const input = document.querySelector("#favchap");
+
+addChapter.addEventListener('click', ()=>{
+    if (input.value = '') {
+        input.focus();
+    }
+    else {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        topTen.append(li);
+    }
+});
+
+deleteButton.addEventListener('click', ()=>{
+    topTen.removeChild(li);
+    input.focus();
+});
+
+input.focus();
+input.value = '';
