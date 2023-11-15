332459752887afd950f4b70abc41ce9dbd6b7ff2
Torin Bolander <bol19001@byui.edu>
2023-11-14 18:06:01 -0700

    nav lines

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -12,25 +12,29 @@ modeButton.addEventListener("click", () => {
 		cards.forEach((card)=>{
 			card.style.border = "1px solid white";
 		})
-		nav.style.border = "1px solid white";
 		navLinks.forEach((link)=>{
 			link.style.color = "#feeed4";
 		})
 		buttons.forEach((button)=>{
 			button.style.background = "#feeed4";
 		})
+		if (screen.width >= 640) {
+			nav.style.border = "1px solid white";
+		}
 	} else {
 		body.style.background = "#feeed4";
 		body.style.color = "black";
 		cards.forEach((card)=>{
 			card.style.border = "1px solid black";
 		})
-		nav.style.border = "1px solid black";
 		navLinks.forEach((link)=>{
 			link.style.color = "black";
 		})
 		buttons.forEach((button)=>{
 			button.style.background = "white";
 		})
+		if (screen.width >= 640) {
+			nav.style.border = "1px solid black";
+		}
 	}
 });
