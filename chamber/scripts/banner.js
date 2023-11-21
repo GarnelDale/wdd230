7d462b8466e1dd25a5a559a7e6c458a7d961e259
Torin Bolander <bol19001@byui.edu>
2023-11-21 16:57:51 -0700

    Week 10 assignment

---
+++ chamber/scripts/banner.js
@@ -0,0 +1,18 @@
+const bannerClose = document.querySelector('.banner button');
+const banner = document.querySelector('.banner');
+const hero = document.querySelector('.hero');
+const day = new Date();
+const weekDay = day.getDay();
+
+//This banner should only appear on Mondays, Tuesdays, and Wednesdays.
+function checkDay() {
+	if (weekDay >= 4 || weekDay == 0)
+		banner.style.display = "none";
+}
+
+bannerClose.addEventListener('click', () => {
+	banner.style.display = "none";
+	hero.style.gridRow = "1/3";
+});
+
+checkDay();
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -9,7 +9,8 @@ const currentTemp = document.querySelector('#weather-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('#weather-desc');
 const windSpeed = document.querySelector('#windspeed');
-
+const windchill = document.querySelector("#windchill");
+const dayInMs = 60 * 60 * 24 * 1000;
 
 async function apiFetch(type,url) {
     try {
@@ -20,7 +21,7 @@ async function apiFetch(type,url) {
             if (type)
                 displayResults(data);
             else
-                getForecast(data);
+                getForecast(data.list);
         } else { 
             throw Error(await response.text());
         }
@@ -29,19 +30,40 @@ async function apiFetch(type,url) {
     }
 }
 
-const dayInMs = 60 * 60 * 24 * 1000;
-
 async function getForecast(data) {
-    const time = new Date();
+    let days = [];
+    let today = new Date();
 
-    data.list.forEach(()=>{
+    for (let i=0; i < 3; i++) {
+        today = new Date(today.getTime() + dayInMs);
+        let tomorrow = today.toISOString().slice(0, 10);
+        days.push(tomorrow);
+    }
         
-    })
+    // Find the object with the highest temperature for each day
+    highTemps = days.map((day) => data
+        .filter(x => x.dt_txt.startsWith(day))
+        .reduce((current, high) => current.main.temp > high.main.temp ? current : high)
+    ) 
+
+    // Find the object with the lowest temperature for each day
+    lowTemps = days.map((day) => data
+        .filter(x => x.dt_txt.startsWith(day))
+        .reduce((current, low) => current.main.temp < low.main.temp ? current : low)        
+    )    
+    
+    // Add the forecast information to the HTML document
+    let weather = document.querySelector("#weather")
+    for (let i=0; i < 3; i++){
+        let forecast = document.createElement("section");
+        forecast.innerHTML = `<h2>${days[i]}</h2><p>High: ${highTemps[i].main.temp.toFixed(0)}&deg;</p><p>Low: ${lowTemps[i].main.temp.toFixed(0)}&deg;</p>`
+        weather.append(newsection)
+    }
+
 }
 
-
 async function displayResults(data) {
-    currentTemp.innerHTML = `Temperature: ${data.main.temp}&deg;F`;
+    currentTemp.innerHTML = `Temperature: ${data.main.temp.toFixed(0)}&deg;F`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
     weatherIcon.setAttribute('src', iconsrc);
@@ -51,16 +73,14 @@ async function displayResults(data) {
     calcWindchill(data.main.temp, data.wind.speed);
 }
 
-apiFetch(true, currentURL);
-apiFetch(false, forecastURL);
-
-const windchill = document.querySelector("#windchill");
-
 function calcWindchill(temp, wind) {
     if (temp > 50 || wind < 3)
         windchill.innerHTML = "Windchill: N/A";
     else {
         let temperature = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp * Math.pow(wind, 0.16));
-        windchill.innerHTML = `Windchill: ${temperature.toFixed(1)}&deg;F`;
+        windchill.innerHTML = `Windchill: ${temperature.toFixed(0)}&deg;F`;
     }
 }
+
+apiFetch(true, currentURL);
+apiFetch(false, forecastURL);
