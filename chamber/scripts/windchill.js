0f0551842c7805dcd34f02978daa1420965a722c
Torin Bolander <bol19001@byui.edu>
2023-11-20 20:43:10 -0700

    Week 07 Course Page

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,15 +1,66 @@
 //15.47, 32.57 placeholder coordinates for the capital of 
 //Sudan for similar weather to Ald-ruhn
+const lat = 15.47;
+const lon = 32.57;
+const APIKey = '2d126ece34c6f363a92656ab0ec6ea42';
+const currentURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`;
+const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`;
+const currentTemp = document.querySelector('#weather-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#weather-desc');
+const windSpeed = document.querySelector('#windspeed');
+
+
+async function apiFetch(type,url) {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); //testing only
+            if (type)
+                displayResults(data);
+            else
+                getForecast(data);
+        } else { 
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const dayInMs = 60 * 60 * 24 * 1000;
+
+async function getForecast(data) {
+    const time = new Date();
+
+    data.list.forEach(()=>{
+        
+    })
+}
+
+
+async function displayResults(data) {
+    currentTemp.innerHTML = `Temperature: ${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+    windSpeed.innerHTML = `Wind Speed: ${data.wind.speed} mph`
+    calcWindchill(data.main.temp, data.wind.speed);
+}
+
+apiFetch(true, currentURL);
+apiFetch(false, forecastURL);
 
 const windchill = document.querySelector("#windchill");
 
 function calcWindchill(temp, wind) {
     if (temp > 50 || wind < 3)
-        windchill.innerHTML = "N/A";
+        windchill.innerHTML = "Windchill: N/A";
     else {
         let temperature = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp * Math.pow(wind, 0.16));
-        windchill.innerHTML = `${temperature.toFixed(1)}&deg;F`;
+        windchill.innerHTML = `Windchill: ${temperature.toFixed(1)}&deg;F`;
     }
 }
-
-calcWindchill(72, 5);
