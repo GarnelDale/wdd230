980cb02bdc2ca44ec67788629af9ccbfb24a5bea
Torin Bolander <bol19001@byui.edu>
2023-12-13 14:32:51 -0700

    update during checks

--- bountiful/scripts/populateFruit.js
+++ bountiful/scripts/populateFruit.js
@@ -2,33 +2,29 @@ const fruityAPI = "data/fruityvice.json";
 const selects = document.querySelectorAll(".fruitSelect");
 
 async function apiFetch(url) {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            //console.log(data); //testing only
-            populate(data);
-        } else { 
-            throw Error(await response.text());
-        }
-    } catch (error) {
-        console.log(error);
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      //console.log(data); //testing only
+      populate(data);
+    } else {
+      throw Error(await response.text());
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function populate(data) {
-    selects.forEach((select) => {
-        let blank = document.createElement("option");
-        blank.value = "";
-        blank.innerText = "Please select a fruit";
-        select.append(blank);
+  selects.forEach((select) => {
     data.forEach((fruit) => {
-        let option = document.createElement("option");
-        option.value = fruit.name;
-        option.innerText = fruit.name;
-        select.append(option);
-    })
-})
+      let option = document.createElement("option");
+      option.value = fruit.name;
+      option.innerText = fruit.name;
+      select.append(option);
+    });
+  });
 }
 
 apiFetch(fruityAPI);
